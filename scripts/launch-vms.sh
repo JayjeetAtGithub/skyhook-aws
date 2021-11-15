233ae50babd473e27d07b02086cd986337f8b3be
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-11-15 11:28:10 -0800

    Improvements to launch_vms.sh

--- scripts/launch-vms.sh
+++ scripts/launch-vms.sh
@@ -7,7 +7,8 @@ case "$1" in
     -s|--spawn)
     spawn_ec2_instances
     ;;
-    -sk|--skip)
+    -p|--prepare)
+    prepare_ec2_instances
     ;;
     *)
     echo "Usage: (-s|--spawn) (-sk|--skip)"
@@ -16,7 +17,7 @@ case "$1" in
 esac
 
 spawn_ec2_instances() {
-    echo "[+] Launching Client ec2 instance"
+    echo "[+] Launching client ec2 instance"
     aws ec2 run-instances \
         --image-id ami-083654bd07b5da81d \
         --count 1 \
@@ -50,19 +51,23 @@ spawn_ec2_instances() {
     aws ec2 describe-instances --output text --query "Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress" > private_ips.txt
 }
 
-ip=($(cat public_ips.txt))
-echo "Client Node: ${ip[0]}"
+prepare_ec2_instances() {
+    echo "[+] Preparing ec2 instances"
+    ip=($(cat public_ips.txt))
+    echo "Client node: ${ip[0]}"
 
-echo "[+] Copying PEM file to client instance"
-scp -i "amazon.pem" amazon.pem ubuntu@${ip[0]}:/home/ubuntu 
+    echo "[+] Copying PEM file to client instance"
+    scp -i "amazon.pem" amazon.pem ubuntu@${ip[0]}:/home/ubuntu 
 
-echo "[+] Copying public and private IPs to client instance"
-scp -i "amazon.pem" public_ips.txt ubuntu@${ip[0]}:/home/ubuntu 
-scp -i "amazon.pem" private_ips.txt ubuntu@${ip[0]}:/home/ubuntu 
+    echo "[+] Copying public and private IPs to client instance"
+    scp -i "amazon.pem" public_ips.txt ubuntu@${ip[0]}:/home/ubuntu 
+    scp -i "amazon.pem" private_ips.txt ubuntu@${ip[0]}:/home/ubuntu 
 
-echo "[+] Copying scripts to the client instance"
-scp -i "amazon.pem" deploy_ceph.sh ubuntu@${ip[0]}:/home/ubuntu 
-scp -i "amazon.pem" passwordless.sh ubuntu@${ip[0]}:/home/ubuntu 
+    echo "[+] Copying scripts to the client instance"
+    scp -i "amazon.pem" deploy_ceph.sh ubuntu@${ip[0]}:/home/ubuntu 
+    scp -i "amazon.pem" passwordless.sh ubuntu@${ip[0]}:/home/ubuntu 
+    ssh -i "amazon.pem" ubuntu@${ip[0]} "chmod +x *.sh"
 
-printf "\n\n\n"
-echo "ssh -i 'amazon.pem' ubuntu@${ip[0]}"
+    printf "\n\n\n"
+    echo "ssh -i 'amazon.pem' ubuntu@${ip[0]}"
+}
