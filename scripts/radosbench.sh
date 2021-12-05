0f51105aa6ee4ed0c58a05e00aac7aa90afa9e89
Jayjeet Chakraborty <jc.github@rediffmail.com>
2021-12-04 21:52:23 -0800

    Modifications

--- scripts/radosbench.sh
+++ scripts/radosbench.sh
@@ -2,22 +2,28 @@
 set -ex
 
 # $1: No. of OSDs
-# $2: Size of objects
-# $3: Experiment name
-# $4: Client
-# $5: Number of threads
+# $2: Experiment name
+# $3: Client
+# $4: Number of threads
 
 do_ssh() {
     ssh -i 'jayjeet2-frankfurt.pem' ubuntu@$1 $2
 }
 
-BASE_DIR=results/${3}/${1}/${2}
+blocksizes=( 4M 8M 16M 32M 64M )
+
+for blksize in "${blocksizes[@]}"
+do
+
+BASE_DIR=results/${2}/${1}/${blksize}
 mkdir -p ${BASE_DIR}
-do_ssh ${4} "rados bench --no-hints -b ${2} -t ${5} -p cephfs_data 60 write --no-cleanup --format=json-pretty" > ${BASE_DIR}/write.json
+do_ssh ${3} "rados bench --no-hints -b ${blksize} -t ${4} -p cephfs_data 60 write --no-cleanup --format=json-pretty" > ${BASE_DIR}/write.json
 ed ${BASE_DIR}/write.json <<< $'1d\nw\nq'
 
-do_ssh ${4} "rados bench --no-hints -t ${5} -p cephfs_data 60 seq --no-cleanup --format=json-pretty" > ${BASE_DIR}/seq.json
+do_ssh ${3} "rados bench --no-hints -t ${4} -p cephfs_data 60 seq --no-cleanup --format=json-pretty" > ${BASE_DIR}/seq.json
 ed ${BASE_DIR}/seq.json <<< $'1d\nw\nq'
 
-do_ssh ${4} "rados bench --no-hints -t ${5} -p cephfs_data 60 rand --no-cleanup --format=json-pretty" > ${BASE_DIR}/rand.json
+do_ssh ${3} "rados bench --no-hints -t ${4} -p cephfs_data 60 rand --no-cleanup --format=json-pretty" > ${BASE_DIR}/rand.json
 ed ${BASE_DIR}/rand.json <<< $'1d\nw\nq'
+
+done
